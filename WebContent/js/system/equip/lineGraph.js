159685af51a58a271e7146d114180bfc676a21ed
xk11223 <xk11223@126.com>
2016-05-09 16:20:56 +0800

    折线图的方法移动位置

--- WebContent/js/system/equip/lineGraph.js
+++ WebContent/js/system/equip/lineGraph.js
@@ -1,19 +1,13 @@
-//var yearMonth = $("#year").val() + $("#month").val();
-//var date = $("#year").val() + $("#month").val() + $("#day").val();
-var date='';
-var graphLegend = $("#graphLegend").val();
-var graphAxis = $("#graphAxis").val();
-var graphSeries = $("#graphSeries").val();
-
+var date = '';
 $(function() {
 	// 基于准备好的dom，初始化echarts实例
 	var myChart = echarts.init(document.getElementById('main'));
-	 var xname = '';
-	 if (date != null && date != "") {
-	 xname = '小时';
-	 } else {
-	 xname = '日';
-	 }
+	var xname = '';
+	if (date != null && date != "") {
+		xname = '小时';
+	} else {
+		xname = '日';
+	}
 	// 指定图表的配置项和数据
 	var option = {
 		title : {
@@ -40,7 +34,7 @@ $(function() {
 		xAxis : {
 			type : 'category',
 			boundaryGap : false,
-			 name : xname,
+			name : xname,
 			axisLabel : {
 				rotate : 30
 			},
@@ -50,54 +44,30 @@ $(function() {
 			type : 'value',
 			name : '液位值'
 		},
-		series : [{
-            name:'储罐#1',
-            type:'line',
-//	            stack: '总量',
-            data:[120, 202, 101, 134, 90, 230, 210,120, 202, 101, 134, 90, 230, 210,120, 202, 101, 134, 90, 230, 210,120, 202, 101, 134, 90, 230, 210,99,23,66]
-        },
-        {
-            name:'储罐#2',
-            type:'line',
-//	            stack: '总量',
-            data:[220, 182, 191, 234, 290,216, 25,220, 182, 191, 234, 290,216, 25,220, 182, 191, 234, 290,216, 25,220, 182, 191, 234, 290,216, 25,41,54,65]
-        }]
+		series : []
 	};
 
-	// 设定要显示横坐标的值
-	graphAxis = graphAxis.substring(1);
-	graphAxis = graphAxis.substring(0, graphAxis.length - 1);
-	graphAxis = graphAxis.replace(" ", "");
-	var axisList = graphAxis.split(",");
-	var resultAxis = new Array();
-	for (var i = 0; i < axisList.length; i++) {
-		resultAxis[i] = axisList[i];
-	}
-	option.xAxis.data = resultAxis;
-//	option.xAxis.data = graphAxis;
+	// 采用ajax异步请求数据
+	$.ajax({
+		type : 'post',
+		url : rootPath + '/gasReport/findGraphData.sxml',
+		dataType : 'json',
+		success : function(result) {
+			if (result) {
+				// 将返回的category和series对象赋值给options对象内的category和series
+				option.xAxis.data = result.axis;
+				option.legend.data = result.legend;
+				var series_arr = result.series;
+				for (var i = 0; i < series_arr.length; i++) {
+					option.series[i] = result.series[i];
+				}
+				// 使用刚指定的配置项和数据显示图表。
+				myChart.setOption(option);
+			}
+		},
+		error : function(errMsg) {
+			console.error("加载数据失败")
+		}
+	});
 
-	// 设定要显示的legend值
-	graphLegend = graphLegend.substring(1);
-	graphLegend = graphLegend.substring(0, graphLegend.length - 1);
-	graphLegend = graphLegend.replace(" ", "");
-	var legendList = graphLegend.split(",");
-	var resultLegend = new Array();
-	for (var i = 0; i < legendList.length; i++) {
-		resultLegend[i] = legendList[i];
-	}
-	option.legend.data = resultLegend;
-//	option.legend.data = graphLegend;
-
-	// 设定所有纵坐标的值
-//	alert(graphSeries.length);
-//	for (var i = 0; i < graphSeries.length; i++) {
-//		option.series[i] = graphSeries[i];
-//	}
-	
-	// 使用刚指定的配置项和数据显示图表。
-	// myChart.hideLoading();
-
-	// 使用刚指定的配置项和数据显示图表。
-	myChart.setOption(option);
-	jsonUrl: rootPath + '/gasEquip/findGraphData.sxml'
 });
--- src/com/banxian/controller/equip/ReportController.java
+++ src/com/banxian/controller/equip/ReportController.java
@@ -1,10 +1,25 @@
 package com.banxian.controller.equip;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.banxian.controller.index.BaseController;
+import com.banxian.entity.equip.EchartsBean;
+import com.banxian.entity.equip.GraphDataMap;
+import com.banxian.entity.equip.Series;
 import com.banxian.util.Common;
+import com.banxian.util.DateUtil;
+import com.banxian.util.SysConsts;
 
 /**
  * 
@@ -19,5 +34,180 @@ public class ReportController extends BaseController {
 	public String listUI() throws Exception {
 		return Common.BACKGROUND_PATH + "/system/monitor/list";
 	}
+	
+	/**
+	 * 折线图
+	 * 
+	 * @param model
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping("lineGraph")
+	public String lineGraph(Model model) throws Exception {
+		model.addAttribute("res", findByRes());
+		
+		// model.addAttribute(SysConsts.ORG_TYPE,
+		// Common.findAttrValue(SysConsts.ORG_TYPE));
+		
+		return Common.BACKGROUND_PATH + "/system/equip/lineGraph";
+	}
+	
+	/**
+	 * 折线图数据
+	 * 
+	 * @param pageNow
+	 * @param pageSize
+	 * @return
+	 * @throws Exception
+	 */
+	@ResponseBody
+	@RequestMapping("findGraphData")
+	public Map<String, Object> findGraphData(ModelMap model) throws Exception {
+		
+		GraphDataMap graphDataMap = getFormMap(GraphDataMap.class);
+		
+		EchartsBean echartBean = dealWithGraphData(graphDataMap);
+		
+		Map<String, Object> dataMap = new HashMap<String, Object>();
+		// 数据分组
+		dataMap.put("legend", echartBean.legend);
+		// 横坐标
+		dataMap.put("axis", echartBean.axis);
+		// 纵坐标
+		dataMap.put("series", echartBean.series);
+		
+		return dataMap;
+	}
 
+	/**
+	 * 处理折线图的数据
+	 * 
+	 * @param graphDataMap
+	 * @return
+	 */
+	private EchartsBean dealWithGraphData(GraphDataMap graphDataMap) {
+
+		// 用户所属站的编号
+		graphDataMap.put(SysConsts.ORG_CODE,
+				Common.findAttrValue(SysConsts.ORG_CODE));
+		// 获取当前的年月
+		Date nowDate = new Date();
+		graphDataMap.put("currYearMonth", DateUtil.format(nowDate, "yyyyMM"));
+		List<GraphDataMap> mapList = GraphDataMap.mapper().findGraphData(
+				graphDataMap);
+		// 数据分组
+		List<String> legend = new ArrayList<String>();
+		// 横坐标
+		List<String> axis = null;
+		// 纵坐标
+		List<Series> series = new ArrayList<Series>();
+		// 页面输入的“日”
+		String day = "";
+		if (graphDataMap.get("day") != null) {
+			day = graphDataMap.get("day").toString();
+		}
+
+		// 页面输入的“月”
+		String month = "";
+		if (graphDataMap.get("month") != null) {
+			month = graphDataMap.get("month").toString();
+		}
+
+		if (day.isEmpty() == false) {
+			axis = new ArrayList<String>(Arrays.asList(new String[] { "01",
+					"02", "03", "04", "05", "06", "07", "08", "09", "10", "11",
+					"12" }));
+		} else {
+			// 获取该月的最后一天
+			String lastDay = null;
+			String tempDay = null;
+			if (month.isEmpty() == false) {
+				tempDay = DateUtil.getMonthEnd(month);
+			} else {
+				tempDay = DateUtil.getMonthEnd(DateUtil.format(new Date()));
+			}
+			lastDay = tempDay.substring(tempDay.length() - 2);
+
+			if ("31".equals(lastDay)) {
+				axis = new ArrayList<String>(Arrays.asList(new String[] { "01",
+						"02", "03", "04", "05", "06", "07", "08", "09", "10",
+						"11", "12", "13", "14", "15", "16", "17", "18", "19",
+						"20", "21", "22", "23", "24", "25", "26", "27", "28",
+						"29", "30", "31" }));
+			} else if ("30".equals(lastDay)) {
+				axis = new ArrayList<String>(Arrays.asList(new String[] { "01",
+						"02", "03", "04", "05", "06", "07", "08", "09", "10",
+						"11", "12", "13", "14", "15", "16", "17", "18", "19",
+						"20", "21", "22", "23", "24", "25", "26", "27", "28",
+						"29", "30" }));
+			} else if ("28".equals(lastDay)) {
+				axis = new ArrayList<String>(Arrays.asList(new String[] { "01",
+						"02", "03", "04", "05", "06", "07", "08", "09", "10",
+						"11", "12", "13", "14", "15", "16", "17", "18", "19",
+						"20", "21", "22", "23", "24", "25", "26", "27", "28" }));
+			} else {
+				axis = new ArrayList<String>(Arrays.asList(new String[] { "01",
+						"02", "03", "04", "05", "06", "07", "08", "09", "10",
+						"11", "12", "13", "14", "15", "16", "17", "18", "19",
+						"20", "21", "22", "23", "24", "25", "26", "27", "28",
+						"29" }));
+			}
+		}
+
+		String currtDevId = "";
+		String lastDevId = "";
+		List<Integer> tempList = new ArrayList<Integer>();
+		String lastDeviceName = null;
+		for (GraphDataMap map : mapList) {
+			currtDevId = map.get("deviceId").toString();
+
+			// 获取当前数据的时间
+			int timeTemp = Integer.valueOf(map.get("currTime").toString());
+
+			// 设备id没变则为同一组数据
+			if (currtDevId != null && currtDevId.equals(lastDevId)) {
+				// 如果当前数据的时间与横坐标一致，则获取纵坐标的值，否则纵坐标设为0
+				if (timeTemp == tempList.size() + 1) {
+					// 设定纵坐标数据
+					tempList.add(Integer.valueOf(map.get("lngLiquidPosition")
+							.toString()));
+				} else {
+					for (int i = tempList.size() + 1; i < timeTemp - 1; i++) {
+						tempList.add(0);
+					}
+					tempList.add(Integer.valueOf(map.get("lngLiquidPosition")
+							.toString()));
+				}
+			} else {
+				// 设备id不同则把设备名称加到数据分组中,把纵坐标数据加到纵坐标中
+				legend.add(map.get("deviceName").toString());
+				if (tempList.size() != 0) {
+					// 当月或当日，在当前时间以后的数据纵坐标都设为0
+					for (int i = tempList.size(); i < axis.size(); i++) {
+						tempList.add(Integer.valueOf("0"));
+					}
+					series.add(new Series(lastDeviceName, "line", tempList));
+					tempList = new ArrayList<Integer>();
+				}
+
+				// 设定纵坐标数据
+				for (int i = 0; i < timeTemp - 1; i++) {
+					tempList.add(Integer.valueOf("0"));
+				}
+				tempList.add(Integer.valueOf(map.get("lngLiquidPosition")
+						.toString()));
+			}
+
+			lastDevId = currtDevId;
+			lastDeviceName = map.get("deviceName").toString();
+		}
+		// 当月或当日，在当前时间以后的数据纵坐标都设为0
+		for (int i = tempList.size(); i < axis.size(); i++) {
+			tempList.add(Integer.valueOf("0"));
+		}
+		series.add(new Series(lastDeviceName, "line", tempList));
+
+		EchartsBean echartsBean = new EchartsBean(legend, axis, series);
+		return echartsBean;
+	}
 }
